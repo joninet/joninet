67b9fa194b5c936fc9b5687b1b73cf53869b9e8d
joninet <joninet@msn.com>
2023-12-11 18:10:58 -0300

    ok

--- pruebas/prueba.py
+++ pruebas/prueba.py
@@ -1,31 +1,26 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        I=1
-        V=5
-        X=10
-        L=50
-        C=100
-        D=500
-        M=1000
-        numero=0
-        for rom in range(len(s)):
-            numNew=s[rom]
-            if numNew == "I":
-                numero+=I
-            elif numNew == "V":
-                numero+=V
-            elif numNew == "X":
-                numero+=X
-            elif numNew == "L":
-                numero+=L
-            elif numNew == "C":
-                numero+=C
-            elif numNew == "D":
-                numero+=D
-            elif numNew == "M":
-                numero+=M
-        return numero
-romano=Solution()
-print(romano.romanToInt("III"))
+    def plusOne(self, digits):
+        result = []
+        carry = 1  # Inicializamos el acarreo con 1, ya que estamos sumando 1
 
-                
+        for x in range(len(digits) - 1, -1, -1):
+            num = digits[x]
+            
+            if carry:
+                num += carry
+                carry = 0  # Reseteamos el acarreo después de sumar
+
+            if num == 10:
+                carry = 1
+                result.insert(0, 0)
+            else:
+                result.insert(0, num)
+
+        if carry:
+            result.insert(0, carry)
+
+        return result
+
+# Ejemplo de uso:
+ejer = Solution()
+print(ejer.plusOne([9, 9]))
