88393f31343883eee140d8fec4c5deae2568c8c0
joninet <joninet@msn.com>
2023-12-11 21:16:05 -0300

    ok

---
+++ Retos_Programacion/2022/01_FIZZBUZZ.py
@@ -0,0 +1,32 @@
+"""/*
+ * Reto #0
+ * EL FAMOSO "FIZZ BUZZ"
+ * Fecha publicación enunciado: 27/12/21
+ * Fecha publicación resolución: 03/01/22
+ * Dificultad: FÁCIL
+ * Enunciado: Escribe un programa que muestre por consola (con un print) los números de 1 a 100 (ambos incluidos y con un salto de línea entre cada impresión), sustituyendo los siguientes:
+ * - Múltiplos de 3 por la palabra "fizz".
+ * - Múltiplos de 5 por la palabra "buzz".
+ * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */"""
+
+for x in range(100):
+    mult3=x//3
+    mult5=x//5
+    if x%3 == 0 and x%5 == 0:
+        print("fizzbuzz")
+    elif x%3 == 0 and x%5 != 0:
+        print("Fizz")
+    elif x%3 != 0 and x%5 == 0:
+        print("Buzz")
+    else:
+        print(x)
+        
+        
---
+++ Retos_Programacion/2022/02_Agrama.py
@@ -0,0 +1,32 @@
+"""/*
+ * Reto #1
+ * ¿ES UN ANAGRAMA?
+ * Fecha publicación enunciado: 03/01/22
+ * Fecha publicación resolución: 10/01/22
+ * Dificultad: MEDIA
+ *
+ * Enunciado: Escribe una función que reciba dos palabras (String) y retorne verdadero o falso (Boolean) según sean o no anagramas.
+ * Un Anagrama consiste en formar una palabra reordenando TODAS las letras de otra palabra inicial.
+ * NO hace falta comprobar que ambas palabras existan.
+ * Dos palabras exactamente iguales no son anagrama.
+ *
+ * Información adicional:
+ * - Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la acomunidad.
+ * - Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+ * - Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+ * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+ *
+ */"""
+contador=0
+palabra1="hola"
+palabra2="hola"
+for x in palabra1:
+    pal1 = palabra1.count(x)
+    pal2 = palabra2.count(x)
+    if pal1 == pal2:
+        contador+=1
+
+if contador == (len(palabra1) and len(palabra2)):
+    print("es un Anagrama")
+else:
+    print("no es un Anagrama")
