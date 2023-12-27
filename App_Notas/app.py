5614b1365ac3b802f77e9dba07040f37d0df2835
joninet <joninet@msn.com>
2023-12-27 20:43:30 -0300

    ok

---
+++ App_Notas/app.py
@@ -0,0 +1,27 @@
+#minuto 19:35
+from flask import Flask, render_template, request
+import config 
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = config.HEX_SEC_KEY
+app.config['MYSQL_HOST'] = config.MYSQL_HOST
+app.config['MYSQL_USER'] = config.MYSQL_USER
+app.config['MYSQL_PASSWORD'] = config.MYSQL_PASSWORD
+app.config['MYSQL_DB'] = config.MYSQL_DB
+
+mysql=MySQL(app)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+
+if __name__ == '__main__':
+    app.run(debug=True)
