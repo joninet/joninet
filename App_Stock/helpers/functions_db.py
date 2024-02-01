2b40620776aca1b89f957e3f22caa9a818b45db8
Jonathan Desplats <124144015+joninet@users.noreply.github.com>
2024-02-01 07:48:25 -0300

    ok

--- App_Stock/helpers/functions_db.py
+++ App_Stock/helpers/functions_db.py
@@ -11,8 +11,22 @@ def insertData(dbconn, table, column, values):
         conn.commit()
         
     except sql.Error as e:
-        print(f"Error al insertar datos: {e}")
+        print(f"Failed to insert data: {e}")
         
     finally:
         if conn:
             conn.close()
+
+import json
+
+def viewRow(dbconn, id, table):
+    db = sql.connect(dbconn)
+    cursor = db.cursor()
+    view = cursor.execute(f"SELECT * FROM {table} WHERE id = ?", (id,)).fetchone()
+    db.close()
+
+    if view:
+        view_dict = dict(zip([desc[0] for desc in cursor.description], view))
+        return json.dumps(view_dict)
+    else:
+        return None 
--- App_Stock/routers/product.py
+++ App_Stock/routers/product.py
@@ -1,20 +1,24 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query, Path
 from models.product import Product
 from config import dbconn
-from helpers.functions_db import insertData
+from helpers.functions_db import insertData, viewRow
 
 router = APIRouter()
 
 @router.post('/products')
 def createProduct(product: Product):
-    columnas = ["name", "code", "stock", "typeProduct", "um"]
-    valores = [product.name, product.code, product.stock, product.typeProduct, product.um]
+    column = ["name", "code", "stock", "typeProduct", "um"]
+    values = [product.name, product.code, product.stock, product.typeProduct, product.um]
 
     try:
-        insertData(dbconn, "products", columnas, valores)
+        insertData(dbconn, "products", column, values)
         return {"message": "Product created successfully"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to create product: {str(e)}")
+    
+@router.get('/products/{id}')
+def getProduct(id: int = Path(gt=0)):
+    return viewRow(dbconn, id, "products")
 
 
 
