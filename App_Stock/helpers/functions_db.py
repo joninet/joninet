fcb81e041de4f5b3910841c075b62ee7d4ee9387
Jonathan Desplats <124144015+joninet@users.noreply.github.com>
2024-02-06 17:23:38 +0000

    ok

--- App_Stock/helpers/functions_db.py
+++ App_Stock/helpers/functions_db.py
@@ -20,23 +20,27 @@ def insertData(dbconn, table, column, values):
             conn.close()
 
 def viewRow(dbconn, id, table):
-    try:
-        db = sql.connect(dbconn)
-        cursor = db.cursor()
-        cursor.execute(f"SELECT * FROM {table} WHERE id = ?", (id,))
-        row = cursor.fetchone()
-        db.close()
+    checkId=printData(dbconn, id, "id", table, "id")
+    if checkId is None:
+        return {"message": "ID not found"}
+    else: 
+        try:
+            db = sql.connect(dbconn)
+            cursor = db.cursor()
+            cursor.execute(f"SELECT * FROM {table} WHERE id = ?", (id,))
+            row = cursor.fetchone()
+            db.close()
 
-        if row:
-            #cursor.description (me muestra la descripcion de cada columna)
-            columns = [desc[0] for desc in cursor.description]
-            #zip convina column y row, y despues con dict las convierte en diccionario a las 2 parejas
-            result = dict(zip(columns, row))
-            return result
-        else:
-            raise HTTPException(status_code=404, detail=f"Row with ID {id} not found in table {table}")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+            if row:
+                #cursor.description (me muestra la descripcion de cada columna)
+                columns = [desc[0] for desc in cursor.description]
+                #zip convina column y row, y despues con dict las convierte en diccionario a las 2 parejas
+                result = dict(zip(columns, row))
+                return result
+            else:
+                raise HTTPException(status_code=404, detail=f"Row with ID {id} not found in table {table}")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
     
 def editRow(dbconn, tabla, columnas, valores, condicion, condicion_valores):
     try:
@@ -76,7 +80,7 @@ def printData(dbconn, valueSearch, columnView, table, columnSearch):
             conn.close()
     
 def deleteRow(dbconn, table, id):
-    checkId=printData(dbconn, id, "id", "products", "id")
+    checkId=printData(dbconn, id, "id", table, "id")
     if checkId is None:
         return {"message": "ID not found"}
     else:
--- App_Stock/routers/entry_product.py
+++ App_Stock/routers/entry_product.py
@@ -1,19 +1,21 @@
 from fastapi import APIRouter, HTTPException, Query, Path
+from typing import Optional
 from models.product import Product
 from config import dbconn
-from helpers.functions_db import printData, insertData
+from helpers.functions_db import printData, insertData, deleteRow, viewRow
 import random
 from datetime import datetime
 
-routerEntry = APIRouter()
+router = APIRouter()
 
-@routerEntry.get('/entryproduct/{code}/{quantity}/{warehouse}/{supplier}/{invoice}')
+@router.get('/entryproduct/{code}/{quantity}/{warehouse}/{supplier}/{invoice}/comment')
 async def entryProduct(
-    code: int = Path(..., title="Código del producto", ge=0),
+    code: int = Path(..., title="Código del producto", ge=0, max_length=20),
     quantity: float = Path(..., title="Cantidad del producto", ge=0),
     warehouse: str = Path(..., title="Almacén"),
     supplier: str = Path(..., title="Proveedor"),
     invoice: str = Path(..., title="Factura"),
+    comment: Optional[str] = None
 ):
     codeSearch=printData(dbconn, code, "code", "products", "code")
 
@@ -22,11 +24,25 @@ async def entryProduct(
         dateEntry=d.strftime("%Y-%m-%d %H:%M:%S")
         name=printData(dbconn, code, "name", "products", "code")
         um=printData(dbconn, code, "um", "products", "code")
-        column = ["date", "code", "name", "um", "quantity", "warehouse", "supplier", "invoice"]
-        values = [dateEntry, code, name, um, quantity, warehouse, supplier, invoice]
+        if comment is not None:
+            column = ["date", "code", "name", "um", "quantity", "warehouse", "supplier", "invoice","comment"]
+            values = [dateEntry, code, name, um, quantity, warehouse, supplier, invoice, comment]
+        else:
+            column = ["date", "code", "name", "um", "quantity", "warehouse", "supplier", "invoice"]
+            values = [dateEntry, code, name, um, quantity, warehouse, supplier, invoice]
         
         insertData(dbconn, "entryproducts", column, values)
-
         return {"message": "Entry successfully"}
     else:
-        return {"message": "no existe el codigo"}
+        return {"message": "The code does not exist"}
+    
+@router.delete('/entryproduct/{id}')
+def deleteEntryProduct(id: int):
+    return deleteRow(dbconn, "entryproducts",id)
+
+@router.get('/entryproduct/{id}')
+def getEntryProduct(id: int = Path(gt=0)):
+    return viewRow(dbconn, id, "entryproducts")
+
+
+
