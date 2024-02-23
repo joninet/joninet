2d5f1baf4975b7e28f8e071759fdd35aa97664ef
joninet <joninet@msn.com>
2024-02-22 22:48:46 -0300

    ok

--- Base_de_Datos/creamos.py
+++ Base_de_Datos/creamos.py
@@ -1,14 +1,28 @@
 import sqlite3
 
-conn=sqlite3.connect('Base_de_Datos/base_datos.db')
-
+conn = sqlite3.connect('Base_de_Datos/base_datos.db')
 cursor = conn.cursor()
 
-cursor.execute("""CREATE TABLE Categoria (
-    id INTEGER PRIMARY KEY,
-    nombre TEXT
+cursor.execute("""CREATE TABLE Ingresos (
+                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+                 fecha DATE NOT NULL,
+                 cantidad REAL NOT NULL,
+                 oc TEXT,
+                 lotes TEXT,
+                 vtos TEXT,
+                 remito TEXT,
+                 proveedor_id INTEGER NOT NULL,
+                 producto_id INTEGER NOT NULL,
+                 estado_id INTEGER NOT NULL,
+                 usuario_id INTEGER NOT NULL,
+                 FOREIGN KEY (proveedor_id) REFERENCES Proveedor(id),
+                 FOREIGN KEY (producto_id) REFERENCES Producto(id),
+                 FOREIGN KEY (estado_id) REFERENCES Estado(id),
+                 FOREIGN KEY (usuario_id) REFERENCES Usuario(id)
+                 
                )
                """)
+
 
 conn.commit()
 conn.close()
--- Base_de_Datos/insertamos.py
+++ Base_de_Datos/insertamos.py
@@ -1,78 +1,38 @@
-import sqlite3
+import sqlite3 as sql
+import json
+from fastapi import HTTPException
+import datetime
 
-"""# Conectar a la base de datos (si no existe, se creará)
-conexion = sqlite3.connect('Base_de_Datos/base_datos.db')
+class HandleDB():
+    def __init__(self):
+        self._con = sql.connect("Base_de_Datos/base_datos.db")
+        self._cur = self._con.cursor()
 
-# Crear un cursor para ejecutar sentencias SQL
-cursor = conexion.cursor()
-
-# Sentencia SQL para insertar datos en la tabla Categoria
-sql_insert = "INSERT INTO Categoria (nombre) VALUES (?)"
-
-# Datos que deseas insertar
-nombres_categorias = ['Electrónica', 'Ropa', 'Hogar', 'Deportes']
-
-# Iterar sobre los nombres de las categorías y ejecutar la sentencia SQL
-for nombre_categoria in nombres_categorias:
-    cursor.execute(sql_insert, (nombre_categoria,))
-
-# Confirmar la transacción
-conexion.commit()
-
-# Cerrar el cursor y la conexión
-cursor.close()
-conexion.close()"""
-
-# Conectar a la base de datos
-conexion = sqlite3.connect('Base_de_Datos/base_datos.db')
-
-# Crear un cursor para ejecutar sentencias SQL
-cursor = conexion.cursor()
-
-# Sentencia SQL para insertar datos en la tabla Producto
-sql_insert_producto = "INSERT INTO Producto (nombre, um, descripcion, categoria_id) VALUES (?, ?, ?, ?)"
-
-# Datos de ejemplo para productos (nombre, um, descripcion, categoria_id)
-datos_productos = [
-    ('Producto1', 'kg', 'Descripción del Producto1', 1),  # Asociado a la categoría con id 1
-    ('Producto2', 'uni', 'Descripción del Producto2', 2),  # Asociado a la categoría con id 2
-    ('Producto3', 'lts', 'Descripción del Producto3', 1),  # Asociado a la categoría con id 1
-    # Puedes agregar más datos aquí
-]
-
-"""# Iterar sobre los datos de ejemplo y ejecutar la sentencia SQL para cada uno
-for producto in datos_productos:
-    cursor.execute(sql_insert_producto, producto)
-
-# Confirmar la transacción
-conexion.commit()
-
-# Cerrar el cursor y la conexión
-cursor.close()
-conexion.close()"""
-
-import sqlite3
-
-conexion = sqlite3.connect('Base_de_Datos/base_datos.db')
-cursor = conexion.cursor()
-
-# Consulta SQL para seleccionar todos los productos con su categoría correspondiente
-sql_select = """
-SELECT Producto.nombre, Producto.um, Producto.descripcion, Categoria.nombre AS categoria
-FROM Producto
-JOIN Categoria ON Producto.categoria_id = Categoria.id
-"""
-
-cursor.execute(sql_select)
-resultados = cursor.fetchall()
-
-for producto in resultados:
-    print("Nombre:", producto[0])
-    print("UM:", producto[1])
-    print("Descripción:", producto[2])
-    print("Categoría:", producto[3])
-    print("-------------------------")
-
-conexion.close()
+    def insertData(self, tabla, columna, valores):
+        try:
+            query = f"INSERT INTO {tabla} ({', '.join(columna)}) VALUES ({', '.join(['?' for _ in valores])})"
+            self._cur.execute(query, valores)
+            self._con.commit()
+        except sql.Error as e:
+            print(f"error al insertar: {e}")
 
 
+    def deleteRow(self, tabla, id):
+        self._cur.execute(f"DELETE FROM {tabla} WHERE id = ?", (id,))
+        self._con.commit()
+        return {"message": "Delet Product successfully"}
+            
+    def __del__(self):
+        self._con.close()
+
+#funcion para insertar datos en una tabla
+#column: nombres de las columnas en formato lista column = ["name", "code"]
+#values: valores de la columna en formato lista values = ["valorName", "valorCode"]
+fecha=datetime.datetime.now()
+fechaHoy=fecha.strftime('%Y-%m-%d %H:%M:%S')
+columna = ["nombre", "direccion", "localidad", "telefono", "email", "moneda", "Fecha"]
+valores = ["AIT", "La Rioja 345", "Buenos Aires", "11-4344446", "consultas@ait.com.ar", "Todas", fechaHoy]
+
+inserto=HandleDB()
+inserto.insertData("Proveedor", columna, valores)
+      
