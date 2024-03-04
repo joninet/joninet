06bd04984edfd54c4c05fbe568efc1e4c94d2854
joninet <joninet@msn.com>
2024-03-03 21:06:48 -0300

    ok

--- App_Stock3/routers/productos.py
+++ App_Stock3/routers/productos.py
@@ -16,20 +16,32 @@ def nuevoProducto(req: Request):
   return template.TemplateResponse("nuevo_producto.html", {"request": req, "categorias": categorias})
 
 @router.post('/productos/crear')
-async def crear_producto(
+async def crearProducto(
     req: Request,
-    nombre: str = Form(...),
-    um: str = Form(...),
-    descripcion: str = Form(...),
-    categoria_id: int = Form(...)):
+    nombre: str = Form(None),
+    um: str = Form(None),
+    descripcion: str = Form(None),
+    categoria_id: int = Form(None)):
 
-    if nombre and um and descripcion and categoria_id:
+    if not (nombre and um and descripcion and categoria_id):
+        error_msg = "Por favor, complete todos los campos."
+        verDB = FuncionesDB()
+        categorias= verDB.mostrarTabla("Categoria")
+        return template.TemplateResponse(
+            "nuevo_producto.html", 
+            {"request": req, "errorIngresoInsumo": error_msg, "nombre": nombre, "um": um, "descripcion": descripcion, "categoria_id": categoria_id, "categorias": categorias}
+        )
 
-      column = ["nombre", "um", "descripcion", "categoria_id"]
-      values = [nombre, um, descripcion, categoria_id]
+    column = ["nombre", "um", "descripcion", "categoria_id"]
+    values = [nombre, um, descripcion, categoria_id]
 
-      insertar = FuncionesDB()
-      insertar.insertarDatos("Producto", column, values)
-      return template.TemplateResponse("datosActualizados.html", {"request": req})
-    else:
-      return template.TemplateResponse("/productos/crear", {"errorIngresoInsumo": "Las credenciales no son correctas o existen campos vacios"})
+    insertar = FuncionesDB()
+    insertar.insertarDatos("Producto", column, values)
+    return template.TemplateResponse("datosActualizados.html", {"request": req})
+
+@router.get("/productos/ver_todos")
+def verProductos(req:Request):
+   verDb=FuncionesDB()
+   productos=verDb.mostrarTabla("Producto")
+   categorias= verDb.mostrarTabla("Categoria")
+   return template.TemplateResponse("productos.html", { "request" : req, "productos": productos, "categorias": categorias })
