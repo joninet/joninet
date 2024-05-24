27d8f5a0813a100fef45f44fdba6a48deb48e7dc
Jonathan Desplats <124144015+joninet@users.noreply.github.com>
2024-05-24 22:52:40 +0000

    ok

---
+++ profesores/templates/static/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+});
