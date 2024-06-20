e186fc4343c50125c83702195886a3e3ab8c2b7e
joninet <joninet@msn.com>
2024-06-19 23:24:48 -0300

    borramos archivos inecesarios

--- profesores/script.js
+++
@@ -1,5 +0,0 @@
-// script.js
-document.getElementById('mobile-menu').addEventListener('click', function() {
-    const sidebar = document.getElementById('sidebar');
-    sidebar.classList.toggle('active');
-});
